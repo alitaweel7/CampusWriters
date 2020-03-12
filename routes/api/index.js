1feac2b86d0601280135d7cd91762ca96db5104a
Ali Mustafa <ali.taweel7@gmail.com>
2020-03-12 14:05:29 -0400

    removed profiles.js from index file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,16 +1,14 @@
 const router = require('express').Router();
 const users = require("./users");
 const interviews = require("./interviews");
-const profiles = require("./profiles"); // added back profiles
 const path = require("path")
 
 //All routes from here begin with /api
 router.use('/users', users);
 router.use('/interviews', interviews);
-router.use('/profiles', profiles); //I added back profiles
 
 router.use((req, res) => {
-    res.sendFile(path.join(__dirname + "../../client/build/static/index.html"))// I think this is my issue I keep getting errors (ask justin)
+    res.sendFile(path.join(__dirname + "../../client/build/static/index.html"))
 })
 
 module.exports = router;
